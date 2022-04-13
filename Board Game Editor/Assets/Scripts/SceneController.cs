2b896d59c15fc34ead4e04cc3603d342018debf7
jonathanLum <loomsden345@gmail.com>
2022-04-13 14:28:14 -0500

    code cleanup and prep for load play board

--- Board Game Editor/Assets/Scripts/SceneController.cs
+++ Board Game Editor/Assets/Scripts/SceneController.cs
@@ -6,42 +6,33 @@ using UnityEngine.Events;
 
 public class SceneController : MonoBehaviour
 {
-    
-    private Scene activeScene;
     public SaveController saveCtrl;
 
     private void Awake() {
         DontDestroyOnLoad(this.gameObject);
-        activeScene = SceneManager.GetActiveScene();
     }
 
     void Start()
     {
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
-        Scene scene = SceneManager.GetSceneByName("MainMenu");
-        activeScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);   
     }
 
     public void GoToEditor(){
         //SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(1));
         SceneManager.LoadScene("BoardEditor", LoadSceneMode.Single);
-        activeScene = SceneManager.GetActiveScene();
+        
     }
     
 
     public void GoToMainMenu(){
         //SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(1));
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
-        activeScene = SceneManager.GetActiveScene();
+        
     }
 
     public void GoToPlayGame(){
         //SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(1));
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
-        activeScene = SceneManager.GetActiveScene();
+        
     } 
-
-    public Scene GetActiveScene(){
-        return activeScene;
-    }
 }
