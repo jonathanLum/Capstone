733a0a54385835fbcb2facb6d177d7a9279b953f
richiestuver <stuverr@oregonstate.edu>
2022-04-13 11:11:43 -0700

    working on UI cleanup for main menu

--- Board Game Editor/Assets/Scripts/UI/MainMenu.cs
+++ Board Game Editor/Assets/Scripts/UI/MainMenu.cs
@@ -7,54 +7,63 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Button newBoard;
-    public GameObject boardOptions;
-    public GameObject boardSelect;
+    // public GameObject boardOptions;
+    // public GameObject boardSelect;
     public Slider boardID;
     public Text boardText;
-    public GameObject playerSelect;
+    // public GameObject playerSelect;
+
+    public GameObject menuEditorOptions;
+    public Button buttonEditorOptions;
 
     GameObject saveCtrl;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        saveCtrl = GameObject.FindGameObjectWithTag("SaveController");
-        boardID.maxValue = saveCtrl.GetComponent<SaveController>().so.saveData.Count - 1;
-        boardID.onValueChanged.AddListener(SetBoardID);
-        SetBoardID(boardID.value);
-        newBoard.onClick.AddListener(saveCtrl.GetComponent<SaveController>().ResetBoardId);
+        // saveCtrl = GameObject.FindGameObjectWithTag("SaveController");
+        // boardID.maxValue = saveCtrl.GetComponent<SaveController>().so.saveData.Count - 1;
+        // boardID.onValueChanged.AddListener(SetBoardID);
+        // SetBoardID(boardID.value);
+        // newBoard.onClick.AddListener(saveCtrl.GetComponent<SaveController>().ResetBoardId);
+
     }
 
-    void SetBoardID(System.Single id){
+    void SetBoardID(System.Single id)
+    {
         saveCtrl.GetComponent<SaveController>().currBoardID = (int)id;
         boardText.text = saveCtrl.GetComponent<SaveController>().so.saveData[(int)id].name;
     }
 
-
-    public void OpenNewBoard(){
-        SceneManager.LoadScene("BoardEditor", LoadSceneMode.Single);
-       
+    public void OpenEditorOptions()
+    {
+        menuEditorOptions.SetActive(true);
     }
+    // public void OpenNewBoard(){
+    //     SceneManager.LoadScene("BoardEditor", LoadSceneMode.Single);
 
-    public void OpenBoardOptions(){
-        boardOptions.SetActive(true);
-    }
+    // }
 
-    public void OpenBoardSelect(){
-        boardSelect.SetActive(true);
-    }
+    // public void OpenBoardOptions(){
+    //     boardOptions.SetActive(true);
+    // }
 
-    public void OpenPlayerSelect(){
-        playerSelect.SetActive(true);
-    }
+    // public void OpenBoardSelect(){
+    //     boardSelect.SetActive(true);
+    // }
 
-    public void BackButton(){
-        boardOptions.SetActive(false);
-        boardSelect.SetActive(false);
-        playerSelect.SetActive(false);
-    }
+    // public void OpenPlayerSelect(){
+    //     playerSelect.SetActive(true);
+    // }
 
-    public void QuitGame(){
+    // public void BackButton(){
+    //     boardOptions.SetActive(false);
+    //     boardSelect.SetActive(false);
+    //     playerSelect.SetActive(false);
+    // }
+
+    public void QuitGame()
+    {
         Application.Quit();
     }
 }
