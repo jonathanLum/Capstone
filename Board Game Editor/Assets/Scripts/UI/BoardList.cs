d32982e05432c59b37bfe83cfb0b302284ad6187
richiestuver <stuverr@oregonstate.edu>
2022-04-17 12:00:13 -0700

    add test listener to click event on boardlistitem

--- Board Game Editor/Assets/Scripts/UI/BoardList.cs
+++ Board Game Editor/Assets/Scripts/UI/BoardList.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 
 public class BoardList : MonoBehaviour
 {
-    public GameObject saveCtrl;
+    public SaveController saveCtrl;
     public GameObject buttonListItem;
     private List<GameBoard> boardList;
+
+    public int currIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        saveCtrl = GameObject.FindGameObjectWithTag("SaveController");
-        boardList = saveCtrl.GetComponent<SaveController>().so.saveData;
+        saveCtrl = GameObject.FindGameObjectWithTag("SaveController").GetComponent<SaveController>();
+        boardList = saveCtrl.so.saveData;
         populateBoardList();
 
     }
@@ -34,19 +36,29 @@ public class BoardList : MonoBehaviour
             // Create a new list item for the board, set as parent to the scrollview content
             Transform scrollViewContent = GameObject.Find("Content").transform;
             GameObject boardListItem = Instantiate(buttonListItem, new Vector3(0, 0, 0), Quaternion.identity, scrollViewContent);
+
             // UI elements use the RectTransform component instead of Transform
             boardListItem.GetComponent<RectTransform>().localPosition = position;
 
-
+            // Component containing the text data to update is TMPro.TextMeshProGUI
+            // https://docs.unity3d.com/Packages/com.unity.textmeshpro@3.0/manual/index.html
             GameObject boardListItemText = boardListItem.transform.GetChild(0).gameObject;
-            Debug.Log(boardListItemText);
             TMPro.TextMeshProUGUI text = boardListItemText.GetComponent<TMPro.TextMeshProUGUI>();
             text.text = board.name;
 
+            boardListItem.GetComponent<Button>().onClick.AddListener(updateCurrentBoard);
             boardListItem.SetActive(true);
-
             position.y -= boardListItem.GetComponent<RectTransform>().sizeDelta.y;
-
         }
+    }
+
+    public void updateCurrentBoard()
+    {
+
+        // determine index of the list based on calculating the difference
+        // between this button and the distance from top of the container.
+
+        Debug.Log(this.gameObject.GetComponent<RectTransform>().anchoredPosition);
+        // saveCtrl.SetBoardID();
     }
 }
