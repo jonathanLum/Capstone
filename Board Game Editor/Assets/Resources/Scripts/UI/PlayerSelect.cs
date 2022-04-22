8cd992f0eede15045ad5fe1e865c8a8ede3440ac
richiestuver <stuverr@oregonstate.edu>
2022-04-21 17:15:45 -0700

    re-implement player count selector

--- Board Game Editor/Assets/Resources/Scripts/UI/PlayerSelect.cs
+++ Board Game Editor/Assets/Resources/Scripts/UI/PlayerSelect.cs
@@ -5,41 +5,75 @@ using UnityEngine.UI;
 
 public class PlayerSelect : MonoBehaviour
 {
-    public Text playerCount;
+    public TMPro.TextMeshProUGUI textPlayerCount;
+    public int playerCount;
     public List<GameObject> pieceSelectors;
-    
+    public GameObject playerCountSelect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCount = 4;
+        SetPlayerCount(playerCount);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    void ChangeDisplay(int value){
-        switch(value){
+    }
+    void ChangeDisplay(int value)
+    {
+        switch (value)
+        {
+            case 1:
+                pieceSelectors[0].SetActive(true);
+                pieceSelectors[1].SetActive(false);
+                pieceSelectors[2].SetActive(false);
+                pieceSelectors[3].SetActive(false);
+                break;
             case 2:
+                pieceSelectors[0].SetActive(true);
+                pieceSelectors[1].SetActive(true);
                 pieceSelectors[2].SetActive(false);
                 pieceSelectors[3].SetActive(false);
                 break;
             case 3:
+                pieceSelectors[0].SetActive(true);
+                pieceSelectors[1].SetActive(true);
                 pieceSelectors[2].SetActive(true);
                 pieceSelectors[3].SetActive(false);
                 break;
             case 4:
+                pieceSelectors[0].SetActive(true);
+                pieceSelectors[1].SetActive(true);
                 pieceSelectors[2].SetActive(true);
                 pieceSelectors[3].SetActive(true);
                 break;
         }
-            
+
     }
 
-    public void UpdateCount(System.Single value){
-        playerCount.text = value.ToString();
+    public void DecrementPlayerCount()
+    {
+        if (playerCount > 1)
+        {
+            SetPlayerCount(playerCount - 1);
+        }
+    }
+
+    public void IncrementPlayerCount()
+    {
+        if (playerCount < 4)
+        {
+            SetPlayerCount(playerCount + 1);
+        }
+    }
+
+    public void SetPlayerCount(int value)
+    {
+        playerCount = value;
+        textPlayerCount.text = value.ToString();
         ChangeDisplay((int)value);
     }
 }
