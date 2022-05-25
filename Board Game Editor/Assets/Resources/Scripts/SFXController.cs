3d6e2211c7c949966775de93def785a1a9566c32
6kimminjun <55810762+6kimminjun@users.noreply.github.com>
2022-05-25 15:33:52 -0400

    Update BG and SFX

---
+++ Board Game Editor/Assets/Resources/Scripts/SFXController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    public AudioSource Audio;
+    public AudioClip Click;
+    public AudioClip diceShake;
+    public AudioClip diceCollision;
+    public AudioClip playerStep;
+
+    public static SFXController SFXInstance;
+
+    private void Awake()
+    {
+        if (SFXInstance != null && SFXInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        SFXInstance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void playClick()
+    {
+        SFXInstance.Audio.PlayOneShot(Click);
+    }
+
+    
+    public void playFootstep()
+    {
+        SFXInstance.Audio.PlayOneShot(playerStep);
+    }
+}
